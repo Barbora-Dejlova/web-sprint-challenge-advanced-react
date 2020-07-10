48d5675428e0529984e3e8cfb0877452e37c3190
Barbora Dejlova <barboradejlova@BarborasMacBook.fios-router.home>
2020-07-10 11:27:21 -0400

    update

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,36 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent, getByText, getByTestId } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+    const { getByText } = render(<CheckoutForm />);
 
-test("form shows success message on submit with form details", () => {});
+    const checkoutHeader = getByText(/checkout form/i);
+});
+
+test("form shows success message on submit with form details", () => {
+  const { getByLabelText, getByTestId, getByDisplayValue } = render(<CheckoutForm />);
+
+  const firstNameInput = getByLabelText(/first name/i);
+  const lastNameInput = getByLabelText(/last name/i);
+  const addressInput = getByLabelText(/address/i);
+  const cityInput = getByLabelText(/city/i);
+  const stateInput = getByLabelText(/state/i);
+  const zipInput = getByLabelText(/zip/i);
+
+  expect(firstNameInput).toBeInTheDocument();
+  expect(lastNameInput).toBeInTheDocument();
+  expect(addressInput).toBeInTheDocument();
+  expect(cityInput).toBeInTheDocument();
+  expect(stateInput).toBeInTheDocument();
+  expect(zipInput).toBeInTheDocument();
+
+  fireEvent.change(firstNameInput, { target: { value: "Barbora" }})
+  fireEvent.change(lastNameInput, { target: { value: "Dejlova" }})
+  fireEvent.change(addressInput, { target: { value: "street" }})
+  fireEvent.change(cityInput, { target: { value: "Leonia" }})
+  fireEvent.change(stateInput, { target: { value: "New Jersey" }})
+  fireEvent.change(zipInput, { target: { value: 7026 }})
+
